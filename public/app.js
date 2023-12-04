fe510b0e3306ac324cf5219106365b7091937010
Moksleivis <tauras.urbonavicius.stud.kitm.lt>
2023-12-04 13:58:02 +0200

    modulinis

--- public/app.js
+++ public/app.js
@@ -7,7 +7,109 @@
   \********************/
 /***/ (() => {
 
+function _typeof(o) { "@babel/helpers - typeof"; return _typeof = "function" == typeof Symbol && "symbol" == typeof Symbol.iterator ? function (o) { return typeof o; } : function (o) { return o && "function" == typeof Symbol && o.constructor === Symbol && o !== Symbol.prototype ? "symbol" : typeof o; }, _typeof(o); }
+function _createForOfIteratorHelper(o, allowArrayLike) { var it = typeof Symbol !== "undefined" && o[Symbol.iterator] || o["@@iterator"]; if (!it) { if (Array.isArray(o) || (it = _unsupportedIterableToArray(o)) || allowArrayLike && o && typeof o.length === "number") { if (it) o = it; var i = 0; var F = function F() {}; return { s: F, n: function n() { if (i >= o.length) return { done: true }; return { done: false, value: o[i++] }; }, e: function e(_e) { throw _e; }, f: F }; } throw new TypeError("Invalid attempt to iterate non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); } var normalCompletion = true, didErr = false, err; return { s: function s() { it = it.call(o); }, n: function n() { var step = it.next(); normalCompletion = step.done; return step; }, e: function e(_e2) { didErr = true; err = _e2; }, f: function f() { try { if (!normalCompletion && it["return"] != null) it["return"](); } finally { if (didErr) throw err; } } }; }
+function _unsupportedIterableToArray(o, minLen) { if (!o) return; if (typeof o === "string") return _arrayLikeToArray(o, minLen); var n = Object.prototype.toString.call(o).slice(8, -1); if (n === "Object" && o.constructor) n = o.constructor.name; if (n === "Map" || n === "Set") return Array.from(o); if (n === "Arguments" || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)) return _arrayLikeToArray(o, minLen); }
+function _arrayLikeToArray(arr, len) { if (len == null || len > arr.length) len = arr.length; for (var i = 0, arr2 = new Array(len); i < len; i++) arr2[i] = arr[i]; return arr2; }
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, _toPropertyKey(descriptor.key), descriptor); } }
+function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); Object.defineProperty(Constructor, "prototype", { writable: false }); return Constructor; }
+function _toPropertyKey(arg) { var key = _toPrimitive(arg, "string"); return _typeof(key) === "symbol" ? key : String(key); }
+function _toPrimitive(input, hint) { if (_typeof(input) !== "object" || input === null) return input; var prim = input[Symbol.toPrimitive]; if (prim !== undefined) { var res = prim.call(input, hint || "default"); if (_typeof(res) !== "object") return res; throw new TypeError("@@toPrimitive must return a primitive value."); } return (hint === "string" ? String : Number)(input); }
+//objektas ir i tam objektui priskiriam jo info
+/*
+function Person (name,email,role){
+    this.name = name;
+    this.email = email;
+    this.role=role;
+//    this.greeting = function(){
+//        alert('Hi im' + this.name + '.')
+    }
 
+
+const userOne = new Person ('jack', 'jack@gmail.com', 'admin');
+
+console.log(userOne)
+
+/////////////////////////////////////////////////////////////////////////////
+
+// kitas budas kaip galima taip padaryti
+/*
+const userOne = new Object();
+userOne.name = 'Jonas';
+userOne.syHi = function (){
+    console.log(this.name)
+};
+console.log(userOne);
+userOne.syHi();
+*/
+/////////////////////////////////////////////////////////////////////////
+//prototipai
+/*
+Person.prototype.greeting= function(){
+    alert('Hi im' + this.name + '.')
+}
+*/
+//pasibaigt namie issivest User
+var User = /*#__PURE__*/function () {
+  //object constructor
+  function User(name, email, role, departament) {
+    _classCallCheck(this, User);
+    this.name = name;
+    this.email = email;
+  }
+  _createClass(User, [{
+    key: "setRole",
+    value: function setRole(role) {
+      //setter
+      this.role = role;
+    }
+  }, {
+    key: "setDepartament",
+    value: function setDepartament(departament) {
+      this.departament = departament;
+    }
+  }, {
+    key: "getUserProfile",
+    value: function getUserProfile() {
+      //getter
+      return [this.name, this.email];
+    }
+  }, {
+    key: "getFullUserData",
+    value: function getFullUserData() {
+      //getter
+      return [this.name, this.email, this.role, this.departament];
+    }
+  }, {
+    key: "displayUI",
+    value: function displayUI(userData) {
+      //display user data in html list
+      var bodyElement = document.querySelector('body');
+      var displayUL = document.createElement('ul');
+      var _iterator = _createForOfIteratorHelper(userData),
+        _step;
+      try {
+        for (_iterator.s(); !(_step = _iterator.n()).done;) {
+          User = _step.value;
+          var _displayLI = document.createElement('li');
+        }
+      } catch (err) {
+        _iterator.e(err);
+      } finally {
+        _iterator.f();
+      }
+      bodyElement.appendChild(displayUL);
+      displayUL.appendChild(displayLI);
+    }
+  }]);
+  return User;
+}();
+var userOne = new User('jack', 'Jack@gmail.com');
+userOne.setRole('admin');
+userOne.setDepartament('IT');
+console.log(userOne.getFullUserData());
+userOne.displayUI(userOne.getFullUserData());
 
 /***/ }),
 
---
+++ src/app.js
@@ -0,0 +1,83 @@
+//objektas ir i tam objektui priskiriam jo info
+/*
+function Person (name,email,role){
+    this.name = name;
+    this.email = email;
+    this.role=role;
+//    this.greeting = function(){
+//        alert('Hi im' + this.name + '.')
+    }
+
+
+const userOne = new Person ('jack', 'jack@gmail.com', 'admin');
+
+console.log(userOne)
+
+/////////////////////////////////////////////////////////////////////////////
+
+// kitas budas kaip galima taip padaryti
+/*
+const userOne = new Object();
+userOne.name = 'Jonas';
+userOne.syHi = function (){
+    console.log(this.name)
+};
+console.log(userOne);
+userOne.syHi();
+*/
+
+/////////////////////////////////////////////////////////////////////////
+
+//prototipai
+/*
+Person.prototype.greeting= function(){
+    alert('Hi im' + this.name + '.')
+}
+*/
+
+//pasibaigt namie issivest User
+
+class User{ //object constructor
+    constructor(name, email, role, departament){
+        this.name = name;
+        this.email = email;
+    }
+    setRole(role){ //setter
+        this.role = role;
+    }
+    setDepartament(departament){
+        this.departament = departament;
+    }
+    getUserProfile(){ //getter
+        return[
+            this.name,
+            this.email
+        ]
+    }
+    getFullUserData(){  //getter
+        return[
+            this.name,
+            this.email,
+            this.role,
+            this.departament
+        ]
+    }
+    displayUI(userData){
+        //display user data in html list
+        const bodyElement = document.querySelector('body');
+        const displayUL = document.createElement('ul');
+        for (User of userData){
+            
+            const displayLI = document.createElement('li');
+        }
+        bodyElement.appendChild(displayUL);
+        displayUL.appendChild(displayLI);
+        
+    }
+}
+
+const userOne =  new User ('jack', 'Jack@gmail.com');
+userOne.setRole ('admin');
+userOne.setDepartament ('IT');
+console.log(userOne.getFullUserData());
+userOne.displayUI(userOne.getFullUserData());
